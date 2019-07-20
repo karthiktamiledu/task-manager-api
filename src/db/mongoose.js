19c389c7a6caf76e7c3f2985ccc54abac76d828d
Karthik <karthiktamiledu@gmail.com>
2019-07-19 19:12:48 -0700

    url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 //process.env.MONGODB_URL
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect('mongodb://taskapp:eigah365ta@cluster0-shard-00-00-mdjws.mongodb.net:27017,cluster0-shard-00-01-mdjws.mongodb.net:27017,cluster0-shard-00-02-mdjws.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
