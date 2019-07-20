90381c07c664628e3172d73ceb1aa5cf4b062c0c
Karthik <karthiktamiledu@gmail.com>
2019-07-19 18:59:30 -0700

    split

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGODB_URL, {
+//process.env.MONGODB_URL
+mongoose.connect('mongodb+srv://taskapp:e1g@h365MA@cluster0-mdjws.mongodb.net/task-manager-api?retryWrites=true', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
